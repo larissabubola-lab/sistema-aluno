deb2b6fab020dd05845906a0fff5809d5d68252d
larissabubola-lab <larissa.bubola@escola.pr.gov.br>
2026-02-26 10:37:17 -0300

    Add files via upload

--- trabalho/app.js
+++ trabalho/app.js
@@ -6,6 +6,16 @@ const readlineSync = require('readline-sync');
 
 let nome = readlineSync.question("Qual o seu nome? ");
 let idade = readlineSync.question("Qual sua idade? ");
-let veracidade = true;
+let veracidade;
 
-console.log(`meu nome é ${nome}, tenho ${idade} anos e essa informacao é ${veracidade}`);
+veracidade = idade>=100? veracidade = false:true
+
+if(veracidade === true){
+    console.log(`meu nome é ${nome}, tenho ${idade} anos e essa informacao é ${veracidade} :)`);
+}
+else{
+    console.log(`meu nome é ${nome}, tenho ${idade} anos e essa informacao é ${veracidade}, seu mentiroso!`);
+}
+
+
+
